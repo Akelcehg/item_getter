bc0c12d73674775edace52473632a3e1181e359d
Akel <akelcheg@gmail.com>
2016-04-24 21:40:14 +0300

    udp

---
+++ modules/parser.js
@@ -0,0 +1,9 @@
+exports.install = function (options) {
+};
+
+exports.uninstall = function (options) {
+};
+
+exports.greeting = function (value) {
+    console.log('From greeting(): ' + value);
+};
